25d4abb810dc3e12fa857142d5bfaa8ad6c98565
jakubkurdziel <qwercy142@gmail.com>
2025-03-22 20:25:41 +0100

    handle caption for quotes

--- src/pages/DailyQuote.tsx
+++ src/pages/DailyQuote.tsx
@@ -1,7 +1,7 @@
 import {useSearchParams} from "react-router-dom";
 import {QuoteProviderProps} from "../components/QuoteProvider.tsx";
 import {ButtonsContainer} from "../components/ShopRef.tsx";
-import {useCallback, useRef, useState, useLayoutEffect} from "react";
+import {useCallback, useLayoutEffect, useRef, useState} from "react";
 import {getAvailableTypes, getQuoteById, getQuotesByType, Quote} from "../infrastructure/qoutes.ts";
 import {Button} from "@/components/ui/button.tsx";
 import QuotesModal from "@/components/QuotesModal.tsx";
@@ -15,7 +15,7 @@ const quoteTypesNameLocalStorageKey = "quoteTypes"
 export const DailyQuote = () => {
     const [searchParams, setSearchParams] = useSearchParams();
 
-    const [quoteText, setQuoteText] = useState<Quote['quote']>('');
+    const [currentQuote, setCurrentQuote] = useState<Quote>(null);
     const [quotes, setQuotes] = useState<Quote[]>([]);
     const [quotesModalOpen, setQuotesModalOpen] = useState(false)
     const [selectedTypes, setSelectedTypes] = useState<QuoteType[]>(getFromLocalStorage<QuoteType[]>(quoteTypesNameLocalStorageKey) || []);
@@ -24,7 +24,7 @@ export const DailyQuote = () => {
     const fetchQuoteById = async (id: string) => {
         const quote = await getQuoteById(id)
 
-        setQuoteText(quote.quote || 'Remember to share your quote with others!');
+        setCurrentQuote(quote || fallbackQuote);
     };
 
     useLayoutEffect(() => {
@@ -65,16 +65,16 @@ export const DailyQuote = () => {
 
     const getRandomQuote = useCallback((quotesArr: Quote[]) => {
         const randomQuote = randomElement(quotesArr);
-        setQuoteText(randomQuote.quote  || 'Remember to share your quote with others!');
+        setCurrentQuote(randomQuote || fallbackQuote);
 
         setSearchParams({q: randomQuote?.id || ''})
     }, [setSearchParams])
 
     return (
         <div ref={ref}>
-            <QuoteProviderProps quote={quoteText} />
+            <QuoteProviderProps quote={currentQuote} caption  />
 
-            <Button onClick={() => setQuotesModalOpen(true)}>Hello</Button>
+            <Button onClick={() => setQuotesModalOpen(true)}> Choose quote types </Button>
 
             <QuotesModal open={quotesModalOpen} onOpenChange={setQuotesModalOpen}>
                 <div className='grid gap-4 grid-cols-[repeat(auto-fill,minmax(100px,1fr))] md:grid-cols-[repeat(4,minmax(100px,1fr))]'>
@@ -94,3 +94,5 @@ export const DailyQuote = () => {
         </div>
     );
 }
+
+const fallbackQuote: Quote = {id: '2137', type: QuoteType.affirmation, quote: 'Remember to share your quote with others!'};
