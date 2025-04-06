dee06bbfa9d1a7dd9a4ede91c9ad1f4bf24b85a5
jakubkurdziel <qwercy142@gmail.com>
2025-04-06 21:17:28 +0200

    add active:scale-110  to nav elements

--- src/components/FooterNavbar.tsx
+++ src/components/FooterNavbar.tsx
@@ -17,15 +17,15 @@ export const FooterNavbar = () => {
     return (
         <div
             className="fixed bottom-0 left-0 w-full bg-black text-white flex justify-around items-center py-3 border-t border-gray-700 z-50 h-[70px]">
-            <NavLink to="/" className={`flex flex-col items-center text-sm`}>
+            <NavLink to="/" className={`flex flex-col items-center text-sm active:scale-110 transition duration-200`}>
                 <FaHome size={22} color={getActiveColor('/')}/>
                 <span className="mt-1">Home</span>
             </NavLink>
-            <NavLink to="/pages/chat" className={`flex flex-col items-center text-sm`}>
+            <NavLink to="/pages/chat" className={`flex flex-col items-center text-sm active:scale-110 transition duration-200`}>
                 <FaCommentDots size={22} color={getActiveColor('/pages/chat')}/>
                 <span className="mt-1">Chat</span>
             </NavLink>
-            <NavLink to="/pages/about" className={`flex flex-col items-center text-sm`}>
+            <NavLink to="/pages/about" className={`flex flex-col items-center text-sm active:scale-110 transition duration-200`}>
                 <FaInfoCircle size={22} color={getActiveColor('/pages/about')}/>
                 <span className="mt-1">About</span>
             </NavLink>
