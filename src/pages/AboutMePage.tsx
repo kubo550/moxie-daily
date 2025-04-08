8cf6e5c574df3bc15407a931d196652c8d1f4fd2
jakubkurdziel <qwercy142@gmail.com>
2025-04-08 16:27:03 +0200

    add moxie video iframe from vimeo

--- src/pages/AboutMePage.tsx
+++ src/pages/AboutMePage.tsx
@@ -2,8 +2,8 @@ import {getCurrentYear} from "@/lib/utils.ts";
 
 export const AboutMePage = () => {
     return (
-        <div>
-            <div className="px-5 text-white">
+        <div className="md:rounded-[15px] shadow-[0_8px_32px_rgba(0,0,0,0.37)] backdrop-blur-[15px] backdrop-contrast-90 backdrop-brightness-110">
+            <div className="px-5 text-white max-w-md">
                 <h1 className="text-2xl md:pt-10 font-bold mb-6 text-left md:py-4" color={"#222"}>About Moxie</h1>
 
                 <div className="w-full max-w-4xl text-justify">
@@ -14,18 +14,19 @@ export const AboutMePage = () => {
                         </p>
                     </section>
 
-                    <section className="">
+                    <section>
                         <h2 className="text-2xl font-semibold mb-4">Watch How Moxie Works</h2>
-                        <div className="max-w-full">
-                            <iframe className="w-full h-64" src="https://www.youtube.com/embed/"
-                                    title="YouTube video player" frameBorder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                    referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                        </div>
+
+                        <iframe
+                            className="w-full"
+                            src="https://player.vimeo.com/video/1072654894?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                            frameBorder="0"
+                            allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+                            title="Moxie"></iframe>
                     </section>
                 </div>
 
-                <footer className="w-full p-4 text-center">
+                <footer className="w-full p-10  text-center">
                     <p>© {getCurrentYear()} MoxieImpact. All Rights Reserved.</p>
                 </footer>
             </div>
