38f5e67477103b8f7aefbd85f2f61bf5e6f4b4eb
Kamila-Kapinos <s221572@student.uek.krakow.pl>
2025-04-15 20:05:49 +0200

    fixing about page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 import logo from '../assets/logo.jpg';
 
 export const Navbar = () => (
-  <div className="absolute top-0 left-0 w-full bg-black py-4 h-16 sm:h-18 md:h-20 z-50">
-    <div className="flex ">
+  <nav className="absolute flex fixed top-0 left-0 right-0 h-[60px] bg-black items-center px-4 shadow-md z-50">
+    <div className="flex">
       <a
         href="https://moxieimpact.com/pages/qr-apparel"
         target="_blank"
@@ -11,9 +11,9 @@ export const Navbar = () => (
         <img
           src={logo}
           alt="Moxie Daily Logo"
-          className="h-16  sm:h-16 md:h-20  max-w-xs mx-auto -mt-4 -ml-2 p-3"
+          className="h-16  sm:h-16 md:h-20  max-w-xs mx-auto -ml-2 p-3"
         />
       </a>
     </div>
-  </div>
+  </nav>
 );
